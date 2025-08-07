9a579e1035a08ee862a7c864025f3a672a896fa1
Nikunj <mnikunj262@gmail.com>
2025-08-08 01:25:01 +0530

    update: items zod validation

---
+++ packages/common/src/types.ts
@@ -0,0 +1,17 @@
+import { email, z } from "zod";
+
+
+export const SignInUserSchema = z.object({
+    email: z.string().min(3).max(20).email(),
+    password: z.string().min(6).max(100)
+})
+
+export const ItemSchema = z.object({
+    name: z.string().min(2),
+    description: z.string().min(20),
+    UseOf: z.string().min(10),
+    url: z.string().url(),
+    shopUrl: z.string().url().optional(),
+    price: z.number().nonnegative("price must be positive").optional(),
+})
+
