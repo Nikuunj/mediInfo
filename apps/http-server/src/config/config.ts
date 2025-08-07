40994e8d33a7a5945cb816fb67d44815f5d1beae
Nikunj <mnikunj262@gmail.com>
2025-08-08 01:26:08 +0530

    update: item post / put / delete and get one / all items end-point

--- apps/http-server/src/config/config.ts
+++ apps/http-server/src/config/config.ts
@@ -3,4 +3,4 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const jwt_secret = process.env.JWT_SECRET || "default-secret";
-export const frontend_url = process.env.FRONTEDN_ENDPOINT || "default-secret";
+export const frontend_url = process.env.FRONTEDN_ENDPOINT || "http://localhost:3000";
--- apps/http-server/src/routes/items.ts
+++ apps/http-server/src/routes/items.ts
@@ -1,20 +1,131 @@
 import { Router } from "express";
 import { middleware } from "../middleware/middleware";
+import { prisma } from "@medi/db/client";
+import { ItemSchema } from "@medi/common/types";
 
 export const itemRoute: Router = Router();
 
 itemRoute.get('/', async (req, res) => {
+    try {
+        const items = await prisma.items.findMany({})
 
+        res.json({
+            items
+        })
+    } catch(e) {
+        res.status(500).json({
+            msg: 'somting went wrong'
+        })
+    }
 })
 
-itemRoute.delete('/', middleware, async (req, res) => {
-    
+itemRoute.get('/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        if(!id) {
+            res.status(401).json({
+                msg: 'id not fond'
+            })
+            return;
+        }
+        const item = await prisma.items.findFirst({
+            where: {
+                id
+            }
+        })
+        res.json({
+            item
+        })
+    } catch (e) {
+        res.status(404).json({
+            msg: 'item not fond with this item id'
+        })
+    }
 })
 
 itemRoute.post('/', middleware, async (req, res) => {
-    
+    const verify = ItemSchema.safeParse(req.body);
+    if(!verify.success) {
+        res.status(422).json({
+            massege: 'plz pass valid input'
+        })
+        return
+    }
+
+    try {
+        await prisma.items.create({
+            data: verify.data
+        })
+
+        res.json({
+            msg: 'item added'
+        })
+    }catch(e) {
+        res.status(500).json({
+            msg: 'somthing worg'
+        })
+    }
 })
 
-itemRoute.put('/', middleware, async (req, res) => {
-    
+itemRoute.put('/:id', middleware, async (req, res) => {
+    const id = req.params.id;
+
+    if(!id) {
+        res.status(401).json({
+            msg: 'id not fond'
+        })
+        return;
+    }
+
+    const verify = ItemSchema.safeParse(req.body);
+    if(!verify.success) {
+        res.status(422).json({
+            massege: 'plz pass valid input'
+        })
+        return
+    }
+    try {
+        await prisma.items.update({
+            where: {
+                id
+            },
+            data: verify.data
+        })
+
+        res.json({
+            msg: 'item updated'
+        })
+    } catch(e) {
+        res.status(404).json({
+            msg: 'fail to update item'
+        })
+    }
+})
+
+itemRoute.delete('/:id', middleware, async (req, res) => {
+    const id = req.params.id;
+
+    if(!id) {
+        res.status(401).json({
+            msg: 'id not fond'
+        })
+        return;
+    }
+
+    try {
+        await prisma.items.delete({
+            where: {
+                id
+            }
+        })
+
+        res.json({
+            msg: 'item deleted'
+        })
+    } catch(e) {
+        res.status(404).json({
+            msg: 'item already deleted'
+        })
+    }
 })
