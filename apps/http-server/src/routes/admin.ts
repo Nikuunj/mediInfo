6f8f7d0461c80914b766d6b1fc38fbd351ec43dd
Nikunj <mnikunj262@gmail.com>
2025-08-07 21:33:16 +0530

    remove: signup end-point

--- apps/http-server/src/routes/admin.ts
+++ apps/http-server/src/routes/admin.ts
@@ -1,14 +1,7 @@
 import { Router } from "express";
-import { middleware } from "../middleware/middleware";
 
 export const admin: Router = Router();
 
+admin.post('/signin', async (req, res) => {
 
-
-admin.post('/signin', middleware, async (req, res) => {
-    
-})
-
-admin.post('/signup', middleware, async (req, res) => {
-    
 })
