29c804a09a9239f01edab60427b6259fe828ef83
Nikunj <mnikunj262@gmail.com>
2025-08-14 21:38:19 +0530

    update: sign end point complete

--- apps/web/app/signin/page.tsx
+++ apps/web/app/signin/page.tsx
@@ -1,26 +1,34 @@
 "use client";
-import { useState } from "react";
+import { signin } from "@/actions/user"
+import { useState } from "react"
+import { useRouter } from "next/navigation"
 
 export default function Signin() {
-  const [form, setForm] = useState({ email: "", password: "" });
+  const [form, setForm] = useState({ email: "", password: "" })
+  const router = useRouter()
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
+    setForm({ ...form, [e.target.name]: e.target.value })
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log("Sign in data:", form);
+  const handleSubmit = async (e: React.FormEvent) => {
+    console.log("Sign in data:", form)
+    const response = await signin(form)
+
+    if(response) {
+      localStorage.setItem('token', response)
+      router.push('../admin')
+    }
   };
 
   return (
     <div className="min-h-screen flex items-center justify-center px-4 sm:px-6 lg:px-8 bg-gradient-to-br from-gray-50 to-gray-200">
       <div className="w-full max-w-sm sm:max-w-md bg-white rounded-2xl shadow-lg p-6 sm:p-8">
         <h2 className="text-xl sm:text-2xl font-bold text-gray-800 mb-6 text-center">
-          Welcome Back 👋
+          Welcome Back Admin👋
         </h2>
 
-        <form onSubmit={handleSubmit} className="space-y-5">
+        <div className="space-y-5">
           {/* Email */}
           <div>
             <label
@@ -63,12 +71,12 @@ export default function Signin() {
 
           {/* Sign in button */}
           <button
-            type="submit"
+            onClick={handleSubmit}
             className="w-full py-2 px-4 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 transition duration-200"
           >
             Sign In
           </button>
-        </form>
+        </div>
       </div>
     </div>
   );
