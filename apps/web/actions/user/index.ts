e6ce69fb8a3e9e2deaa32ba2e626677ec0483b01
Nikunj <mnikunj262@gmail.com>
2025-08-14 21:40:13 +0530

    update: user sign action

--- apps/web/actions/user/index.ts
+++ apps/web/actions/user/index.ts
@@ -2,19 +2,21 @@ import { signinType } from '@medi/common/types'
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
-export async function signin({ email, password }: signinType): Promise<boolean> {
+export async function signin({ email, password }: signinType): Promise<string> {
      try {
-          const { data } = await axios.post('http://localhost:3001/v1/auth/signin', {
+          const reponse = await axios.post('http://localhost:3002/v1/admin/signin', {
                email,
                password
           })
-          localStorage.setItem('token', data.token);
-          return true;
+
+          
+          return reponse.data.token;
      } catch (e) {
           if (axios.isAxiosError(e) && e.response) {
-               console.error(e.response.data.massege)
-               toast.error(e.response.data.massege)
+               console.error(e.response);
+               console.error(e.response.data.msg)
+               toast.error(e.response.data.msg ?? e.response.data.massege)
           }
-          return false;
+          return '';
      }
 }
