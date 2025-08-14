c844c54a3bf1dd6e32548ebb2e65f2fd5a896b83
Nikunj <mnikunj262@gmail.com>
2025-08-14 21:40:56 +0530

    update: create items and get items action fn

--- apps/web/actions/item/index.ts
+++ apps/web/actions/item/index.ts
@@ -1,5 +1,4 @@
-import { itemType } from '@medi/common/types'
-import axios from 'axios';
+import axios from 'axios'
 import toast from 'react-hot-toast';
 
 const authHeader = () => ({ 
@@ -33,8 +32,8 @@ export async function deleteItem(id: string): Promise<boolean> {
         return true;
     } catch (e) {
         if (axios.isAxiosError(e) && e.response) {
-            console.error(e.response.data.massege)
-            toast.error(e.response.data.massege)
+            console.error(e.response.data.msg ?? e.response.data.massege)
+            toast.error(e.response.data.msg ?? e.response.data.massege)
         }
         return false;
     }
@@ -46,24 +45,29 @@ export async function updateItem(id: string): Promise<boolean> {
         return true;
     } catch (e) {
         if (axios.isAxiosError(e) && e.response) {
-            console.error(e.response.data.massege)
-            toast.error(e.response.data.massege)
+            console.error(e.response.data.msg ?? e.response.data.massege)
+            toast.error(e.response.data.msg ?? e.response.data.massege)
         }
         return false;
     }
 }
 
-export async function createNewItem(): Promise<boolean> {
+export async function createNewItem(submitData: any) {
     try {
-        const { data } = await axios.post('http://localhost:3002/v1/item', { }, authHeader())
-         
-        return true;
+        console.log(submitData);
+        
+        const response = await axios.post('http://localhost:3002/v1/item', submitData , authHeader())
+        return { auth: true, create: true};
     } catch (e) {
         if (axios.isAxiosError(e) && e.response) {
-            console.error(e.response.data.massege)
-            toast.error(e.response.data.massege)
+            console.error(e.response.data.msg ?? e.response.data.massege)
+            toast.error(e.response.data.msg ?? e.response.data.massege)
+
+            if(e.response.status === 401) {
+                return { auth: false, create: false};
+            }
         }
-        return false;
+        return { auth: true, create: false};;
     }
 }
 
