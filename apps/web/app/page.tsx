62319da57ef18d8d2486dba9dd79cec0ee0650a7
Nikunj <mnikunj262@gmail.com>
2025-08-21 00:40:17 +0530

    update: tailwind merge and assets

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,22 @@
+import About from "@/components/About";
+import CTA from "@/components/CTA";
+import Features from "@/components/Features";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
 
 export default function Home() {
-    return <div>
-        hi there
+    return (
+        <div className="min-h-screen bg-background">
+            <Header />
+            <main>
+                <Hero />
+                <Features />
+                <About />
+                <CTA />
+            </main>
+            <Footer />
         </div>
-
+    )
 }
