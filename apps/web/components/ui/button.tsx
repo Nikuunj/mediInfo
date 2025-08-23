ea83bca52994a64a1e5e0dec03f3776434e15172
Nikunj <mnikunj262@gmail.com>
2025-08-24 00:01:27 +0530

    happy bdy and update: button style

--- apps/web/components/ui/button.tsx
+++ apps/web/components/ui/button.tsx
@@ -9,11 +9,11 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-gradient-to-r from-[#4285F4] to-[#4FC3F7] text-white hover:opacity-90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+        outline: 
+          "border border-zinc-300  bg-white hover:bg-[#4285F4] hover:text-white",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
